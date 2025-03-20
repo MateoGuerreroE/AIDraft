f4afdad93842a3e83bd7a6e6549fcfb0a63c12f0
Mateo Guerrero <mateof1223@gmail.com>
2025-03-20 11:53:25 -0500

    Implement Nestjs JWT Module On Identity

--- aidraft-server/src/modules/Identity.module.ts
+++ aidraft-server/src/modules/Identity.module.ts
@@ -3,14 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRecord } from 'src/data';
 import { LoggingModule } from './Logging.module';
 import { ConfigModule } from './Config.module';
-import { JwtService } from '@nestjs/jwt';
-import { UserService } from 'src/services';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigService, UserService } from 'src/services';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRecord]),
     LoggingModule,
     ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.getJwtSecret(),
+        signOptions: { expiresIn: configService.getDefaultExpiration() },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [],
   providers: [UserService, JwtService],
