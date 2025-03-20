14b7b8e409cb38d75f4f7392756b86881d5f1b49
Mateo Guerrero <mateof1223@gmail.com>
2025-03-20 14:47:21 -0500

    Upgrade NextJs Project To 14.2.5

---
+++ airdraft-client/app/providers.tsx
@@ -0,0 +1,6 @@
+import { HeroUIProvider } from "@heroui/react";
+import React from "react";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return <HeroUIProvider>{children}</HeroUIProvider>;
+}
