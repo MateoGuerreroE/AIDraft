cdb99cffca3d8362084b08cca03569aa9b5e37cf
Mateo Guerrero <mateof1223@gmail.com>
2025-03-20 12:04:09 -0500

    Connect Firebase Service Implementation To Module

--- aidraft-server/src/app.module.ts
+++ aidraft-server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DataModule } from './data/data.module';
 import { ContentModule } from './modules/Content.module';
+import { IdentityModule } from './modules/Identity.module';
 
 @Module({
-  imports: [DataModule, ContentModule],
+  imports: [DataModule, ContentModule, IdentityModule],
   controllers: [],
   providers: [],
 })
