ce10f7ce3fed318acbbeb232301bc3c1b7913af3
Mateo Guerrero <mateof1223@gmail.com>
2025-03-20 16:33:50 -0500

    Extend Button Component With Base Response

--- airdraft-client/components/ButtonComponent.tsx
+++ airdraft-client/components/ButtonComponent.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@heroui/react";
-import React from "react";
+"use client";
+import { Button, Spinner } from "@heroui/react";
+import React, { useState } from "react";
 
 type Props = {
   text: string;
   hexBg?: string;
-  action?: (...args: string[]) => unknown;
+  action?: (...args: string[]) => unknown | Promise<unknown>;
 };
 
 export default function ButtonComponent({
@@ -12,17 +13,28 @@ export default function ButtonComponent({
   hexBg = "#ffffff",
   action,
 }: Props) {
+  const [loading, isLoading] = useState<boolean>(false);
+  const handleAction = async () => {
+    if (action) {
+      isLoading(true);
+      try {
+        await action();
+      } catch {
+        window.alert("ERROR");
+      } finally {
+        isLoading(false);
+      }
+    }
+  };
   return (
     <Button
       className="font-semibold text-lg p-7 font-outfit"
       radius="full"
       fullWidth
       style={{ backgroundColor: `${hexBg}` }}
-      onPress={() =>
-        action ? action() : console.log("No action defined for this button")
-      }
+      onPress={() => handleAction()}
     >
-      {text}
+      {loading ? <Spinner /> : text}
     </Button>
   );
 }
