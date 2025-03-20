4c3f84e6ce8b6dd0dd5ce95bd59bb7bb87055f90
Mateo Guerrero <mateof1223@gmail.com>
2025-03-20 10:39:18 -0500

    Implement Summary Service And Abstract Generative Service

---
+++ aidraft-server/src/services/Summary.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { NoteRecord, SummaryRecord } from 'src/data';
+import {
+  BadRequestError,
+  GenerativeAIService,
+  NotFoundError,
+  SummaryType,
+} from 'src/types';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class SummaryService {
+  constructor(
+    @InjectRepository(SummaryRecord)
+    private readonly summaryRepository: Repository<SummaryRecord>,
+    @InjectRepository(NoteRecord)
+    private readonly noteRepository: Repository<NoteRecord>,
+    private readonly generativeSummaryService: GenerativeAIService,
+  ) {}
+
+  async createSummary(
+    noteId: string,
+    type: SummaryType,
+    customInstructions?: string,
+  ): Promise<SummaryRecord> {
+    const note = await this.noteRepository.findOneBy({ noteId });
+    if (!note) {
+      throw new NotFoundError('Note not found');
+    }
+    const noteContent = note.content;
+    if (!noteContent.length) throw new BadRequestError('Note is empty');
+    const summary = await this.generativeSummaryService.generateSummary(
+      noteContent,
+      type,
+      customInstructions,
+    );
+    const summaryInstance = this.summaryRepository.create({
+      note: { noteId },
+      body: summary,
+      type,
+    });
+    return this.summaryRepository.save(summaryInstance);
+  }
+}
--- aidraft-server/src/services/index.ts
+++ aidraft-server/src/services/index.ts
@@ -2,3 +2,4 @@ export * from './Config.service';
 export * from './Logging.service';
 export * from './User.service';
 export * from './Note.service';
+export * from './Summary.service';
